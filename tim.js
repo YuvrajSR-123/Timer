72e1bc799311961a4d199726852d47747ceee0b7
Yuvraj Srivastava <85051109+YuvrajSR-123@users.noreply.github.com>
2021-05-30 12:21:51 +0530

    Add files via upload

---
+++ tim.js
@@ -0,0 +1,25 @@
+function starttime()
+{
+	er=setInterval("fun11()",1000)
+}
+var sec=59;
+var min=2;
+var er;
+function fun11()
+{
+	if(min==0 && sec==0)
+	{
+		clearIntervals(er);
+	}
+	else if(sec==0)
+	{
+		min--;
+		sec=59;
+	}
+	
+	realtimer.innerHTML=min+":"+sec+" min left";
+	
+		sec--;
+	
+
+}
